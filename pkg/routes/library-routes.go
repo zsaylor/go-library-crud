28a0605dc1fa885d8a9bc5f34f672cbf8928f438
zsaylor <zchrysaylor@gmail.com>
2022-03-11 15:37:24 +0100

    Include versioning in endpoints

--- pkg/routes/library-routes.go
+++ pkg/routes/library-routes.go
@@ -7,9 +7,9 @@ import (
 )
 
 var RegisterLibraryRoutes = func(router *mux.Router) {
-	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/api/v1/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/api/v1/book/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/api/v1/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/api/v1/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/api/v1/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
